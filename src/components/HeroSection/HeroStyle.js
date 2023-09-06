4ad78dfc61f961ce29544a7c21d325dd633fd69c
dua1999 <94859612+dua1999@users.noreply.github.com>
2023-09-06 22:43:20 +0400

    HeroStyle.js

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-// import _default from "../../themes/default";
+import React from "react";
+
 
 export const HeroContainer = styled.div`
   background: ${({ theme }) => theme.card_light};
