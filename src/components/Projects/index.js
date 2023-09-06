d6eaf3bfb73771d67c211aa942be59eb81eafbc0
dua1999 <94859612+dua1999@users.noreply.github.com>
2023-09-06 22:47:04 +0400

    index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
-import ProjectCards from './ProjectsCards'
-import { projects } from '../../data/constants'
+import { useState } from 'react';
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle';
+import ProjectCard from './ProjectsCards';
+import { projects } from '../../data/constants';
 
+/* eslint-disable react/prop-types */
 
-const Projects = ({openModal,setOpenModal}) => {
+const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState('all');
   return (
     <Container id="projects">
@@ -14,39 +14,52 @@ const Projects = ({openModal,setOpenModal}) => {
         <Desc>
           I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
         </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'all' ?
-            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-            :
-            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-          }
+        <ToggleButtonGroup>
+          {toggle === 'all' ? (
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle('all')}>
+              All
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-            :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-          }
+          {toggle === 'web app' ? (
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>
+              WEB APP&apos;S
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>
+              WEB APP&apos;S
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-            :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          }
+          {toggle === 'android app' ? (
+            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>
+              ANDROID APP&apos;S
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="android app" onClick={() => setToggle('android app')}>
+              ANDROID APP&apos;S
+            </ToggleButton>
+          )}
+          <Divider />
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === 'all' && projects.map((project) => (
-              <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+          {toggle === 'all' &&
+            projects.map((project) => (
+              <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
             ))}
           {projects
             .filter((item) => item.category === toggle)
             .map((project) => (
-              <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+              <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
             ))}
         </CardContainer>
-        <ProjectCards/>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
