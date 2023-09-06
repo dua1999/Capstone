db74e356f332cf5ef8273ad1580a038d59de6309
dua1999 <94859612+dua1999@users.noreply.github.com>
2023-09-06 22:45:01 +0400

    index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -29,7 +29,6 @@ const Navbar= ( )=> {
                 <NavLink href="#skills">Skills</NavLink>
                 <NavLink href="#experience">Experience</NavLink>
                 <NavLink href="#projects">Projects</NavLink>
-                <NavLink href="#education">Education</NavLink>
 
             </NavItems>
             <ButtonContainer>
